714cdb527c500dc362840994c29c4348b78da570
Sigurt <sigurt91@gmail.com>
2013-11-06 12:48:33 +0100

    modified:   list/Makefile

--- oo2/prodcons.c
+++ oo2/prodcons.c
@@ -1,26 +1,32 @@
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "list/list.h"
 
 typedef struct runpars{
-	int offset;
+	int start;
+	int end;
 } runpars;
 
 void *producer(void *params);
 void *consumer(void *params);
 void runThread(pthread_t *tidP, runpars *params);
 pthread_t* spawnNThreads(int n, void*(*fun) (void *));
+void sleeper(float waitMs);
 int prodN;
 int consN;
 int bufferSize;
 int productsN;
 int producerLoad;
+int consumerLoad;
+float SLEEP_TIME_MS = 1.0;
 List *buffer;
 sem_t empty, full;
 
 int main(int argc, char* argv[]){
+	setbuf(stdout,NULL);
 	if(argc != 5){
 		fprintf(stderr, "usage: prodcons [procucers] [consumers] [bufferSize] [products]\n");
 		return -1;
@@ -47,43 +53,49 @@ int main(int argc, char* argv[]){
 	consN = atoi(argv[2]);
 	bufferSize = atoi(argv[3]);
 	productsN = atoi(argv[4]);
-	producerLoad = prodN / productsN;
+	producerLoad = productsN / prodN;
+	consumerLoad = productsN / consN;
+	printf("Starting! Here are the stats\n"
+			"prodN: %i\n"
+			"consN: %i\n"
+			"buffer: %i\n"
+			"products: %i\n"
+			"produderload: %i\n"
+			"consumerload: %i\n",
+			prodN, consN, bufferSize, productsN, producerLoad, consumerLoad);
 
-	sem_init(&full, 0, bufferSize);
+	sem_init(&full, 0, 0);
 	sem_init(&empty, 0, bufferSize);
+	srand(time(NULL));
 
-	pthread_t *ids = spawnNThreads(prodN, producer);
+	pthread_t *consIds = spawnNThreads(consN, consumer);
+	pthread_t *prodIds = spawnNThreads(prodN, producer);
 	int i;
 	for(i=0; i<prodN; i++){
-		pthread_join(ids[i], NULL);
+		pthread_join(prodIds[i], NULL);
 	}
-	free(ids);
-	//int upper = atoi(argv[1]);
-	//int nThreads = atoi(argv[2]);
-	//int stepSize = upper/nThreads;
-	//struct runpars pars[nThreads];
-	//int i;
-	//for (i=0; i<nThreads; i++){
-	//	int first = i * stepSize;
-	//	int last = (i == nThreads-1) ? upper : (i+1) * stepSize -1;
-	//	pars[i] = (runpars) {.lower = first, .upper = last, .res = 0.0};
-	//	//printf("%d to %d\n", pars[i].lower, pars[i].upper);
-	//}
-	//spawnNThreads(nThreads, pars);
-	//float sum = 0.0; //for (i=0; i<nThreads; i++){
-	//	sum += pars[i].res;
-	//}
-	//printf("sum of squares 0 though %d = %f\n", upper, sum);
-
+	for(i=0; i<consN; i++){
+		pthread_join(consIds[i], NULL);
+	}
+	free(prodIds);
+	free(consIds);
 	return 0;
 }
 
-pthread_t* spawnNThreads(int n, void*(*fun) (void *)){
+pthread_t* spawnNThreads(int n, void *(*fun)(void *)){
 	pthread_t *ids = (pthread_t*) malloc(sizeof(pthread_t)*n);
 	int i;
 	for (i=0; i<n; i++){
 		struct runpars *par = malloc(sizeof(runpars));
-		par->offset = 1;
+		if(fun==producer){
+		par->start = producerLoad*i;
+		par->end = i==n-1 ? productsN -1 : (producerLoad * (i+1))-1;
+		printf("produce steps: %i - %i\n", par->start, par->end);
+		}else{
+		par->start = consumerLoad*i;
+		par->end = i==n-1 ? productsN -1 : (consumerLoad * (i+1))-1;
+		printf("consume steps: %i - %i\n", par->start, par->end);
+		}
 		pthread_create(&ids[i], NULL, fun, par);
 	}
 	return ids;
@@ -92,11 +104,17 @@ pthread_t* spawnNThreads(int n, void*(*fun) (void *)){
 void *producer(void *params){
 	struct runpars* pars = (struct runpars*) params;
 	int i;
-	for(i = 0; i < pars->offset; i++){
+	for(i = pars->start; i < pars->end; i++){
 		Node *n = node_new();
 		n->elm = (void*) i;
+		sem_wait(&empty);
 		list_add(buffer, n);
-		printf("producer made item %i\n", (int) n->elm);
+		sem_post(&full);
+		sleeper(SLEEP_TIME_MS);
+		int fullC,emptyC;
+		sem_getvalue(&full,&fullC);
+		sem_getvalue(&empty,&emptyC);
+		printf("producer %i made item %i, full: %i, empty: %i\n", pars->start/producerLoad, (int) n->elm, fullC, emptyC);
 	}
 	free(pars);
 	pthread_exit(0);
@@ -105,10 +123,22 @@ void *producer(void *params){
 void *consumer(void *params){
 	struct runpars* pars = (struct runpars*) params;
 	int i;
-	for(i = 0; i < pars->offset; i++){
+	for(i = pars->start; i < pars->end; i++){
+		sem_wait(&full);
 		Node *n = list_remove(buffer);
-		printf("consumer ate item %i\n", (int) n->elm);
+		sem_post(&empty);
+		sleeper(SLEEP_TIME_MS);
+		int fullC,emptyC;
+		sem_getvalue(&full,&fullC);
+		sem_getvalue(&empty,&emptyC);
+		printf("consumer ate item %i, full: %i, empty: %i\n", (int) n->elm, fullC, emptyC);
 	}
 	free(pars);
 	pthread_exit(0);
+}
+
+void sleeper(float waitMs){
+	//random value between 0 and waitMs
+	waitMs = (float)rand() * waitMs / (float)RAND_MAX;
+	usleep((int)waitMs * 1000); //times 1000 for microseconds;
 }
